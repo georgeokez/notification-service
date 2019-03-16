7de0ddeddf9a2c2b8b6bb41b7b1e3e1cbb7cd35f
george <gokereka@teamapt.com>
2019-03-16 08:55:22 +0100

    Initial commit

---
+++ src/main/java/com/george/dev/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.george.dev.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
